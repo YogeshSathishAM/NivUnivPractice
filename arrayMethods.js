8d548fb7fe4a42c288e6ab0033601d2dc2bc2777
YogeshSathishAM <yogesh.ajilamogaru@niveussolutions.com>
2024-03-07 00:39:16 +0530

    chnages madel

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,5 +1,5 @@
 console.log("array methods")
-
+console.log("array change for testing push") // check this in pushing
 let arr = [2,8,5,1,6.9,83,44]
 let aee1 = ["teye","hdu","uqi"]
 console.log(arr.concat(arr,aee1)) // concat 2 arrays
